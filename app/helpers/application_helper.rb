fb4384c5d2d66e630417a4e684e6bc29b37c4608
Anirudh <gm.anirudh@gmail.com>
2013-11-16 14:19:16 -0500

    Removed the static_pages contoller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    #Returns full title on a per-page basis
+	def full_title(page_title)
+		base_title = "Interngration"
+		if(page_title).empty?
+			base_title
+		else
+			"#{page_title} | #{base_title}"
+	    end
+	end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,37 +5,37 @@ describe "StaticPages" do
 	describe "Landing page" do
 		it "should have content 'Multimedia platform'" do
 		  visit '/static_pages/home'
-		  expect(page).to have_content('Multimedia platform')
+		  expect(page).to have_content('Multimedia platform to connect startups with students')
 		end
 
-	    it "should have title 'Multimedia platform'" do
+	    it "should have base title 'Interngration'" do
 	    	visit '/static_pages/home'
-	    	expect(page).to have_title('Multimedia platform to connect startups with students - Interngration')
+	    	expect(page).to have_title('Interngration')
 	    end
 
     end
 
     describe "Contact Page" do
-		it "should have the title 'Contact Us- Interngration'" do
+		it "should have the title 'Contact Us | Interngration'" do
 		  visit '/static_pages/contact'
 		  expect(page).to have_content('Contact')
 		end
 
 	    it "should have title 'Contact Us'" do
 	    	visit '/static_pages/contact'
-	    	expect(page).to have_title('Contact Us - Interngration')
+	    	expect(page).to have_title('Contact Us | Interngration')
 	    end
     end
 
     describe "About Us Page" do
-		it "should have the title 'About Us - Interngration'" do
+		it "should have the title 'About Us | Interngration'" do
 		  visit '/static_pages/about'
 		  expect(page).to have_content('About')
 		end
 
 	    it "should have title 'About Us'" do
 	    	visit '/static_pages/about'
-	    	expect(page).to have_title('About Us - Interngration')
+	    	expect(page).to have_title('About Us | Interngration')
 	    end
     end
 
