c6ed40ae1d1ef43a5df4deb925bc633bfd8f8282
Anirudh <gm.anirudh@gmail.com>
2013-11-18 23:44:49 -0500

    Updated routes and partials

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 describe "StaticPages" do
 
 	describe "Landing page" do
-		it "should have content 'Multimedia platform'" do
-		  visit '/static_pages/home'
-		  expect(page).to have_content('Multimedia platform to connect startups with students')
+		it "should have content 'multimedia platform'" do
+		  visit root_path
+		  expect(page).to have_content('multimedia platform')
 		end
 
 	    it "should have base title 'Interngration'" do
-	    	visit '/static_pages/home'
+	    	visit root_path
 	    	expect(page).to have_title('Interngration')
 	    end
 
@@ -17,24 +17,24 @@ describe "StaticPages" do
 
     describe "Contact Page" do
 		it "should have the title 'Contact Us | Interngration'" do
-		  visit '/static_pages/contact'
+		  visit contact_path
 		  expect(page).to have_content('Contact')
 		end
 
 	    it "should have title 'Contact Us'" do
-	    	visit '/static_pages/contact'
-	    	expect(page).to have_title('Contact Us | Interngration')
+		  visit contact_path
+	      expect(page).to have_title('Contact Us | Interngration')
 	    end
     end
 
     describe "About Us Page" do
 		it "should have the title 'About Us | Interngration'" do
-		  visit '/static_pages/about'
+		  visit about_path
 		  expect(page).to have_content('About')
 		end
 
 	    it "should have title 'About Us'" do
-	    	visit '/static_pages/about'
+	    	visit about_path
 	    	expect(page).to have_title('About Us | Interngration')
 	    end
     end
